840b097d45977b4a716f8f2490f30fb070dd5f06
Froyd <gabaritocrypter@gmail.com>
2020-05-18 11:18:34 -0300

    first commit

---
+++ filtrarSenhas.py
@@ -0,0 +1,24 @@
+import re
+
+senhasValidas = []
+
+def filtro(anypass):
+  pattern = re.compile(r'''^.*(?=.{1,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!_.'@#$%*?;>)"(<^&+=]).*$''')
+  findings = re.findall(pattern, anypass)
+  if len(findings)>0:
+    senhasValidas.append(anypass)
+  
+def main(senhasValidas):
+  with open('senhas.txt', 'r') as f:
+    for i in f:
+      filtro(i)
+    f.close()
+  
+  with open('senhasValidas.txt', 'w') as sV:
+    for i in senhasValidas:
+      sV.write(i)
+    sV.close()
+  
+  
+main(senhasValidas)
+  
